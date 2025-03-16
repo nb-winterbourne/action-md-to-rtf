a91f0ac2488b43f19e93bf6debd0291859812223
nataliebrooks <natalie.brooks@dexcom.com>
2025-03-16 10:08:23 -0600

    list files

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import pypandoc
 
+class File:
+    def __init__(self, path):
+        self.path = path.as_posix()
+        self.name = self._extract_name()
+
+    def _extract_name(self):
+        name = self.path.split('.')[:-1]
+        if len(name) > 1:
+            name = ".".join(name)
+        else:
+            name = name[0]
+        return name
+
+def get_manu_files():
+    files = []
+    paths = pathlib.Path('manuscript')
+    for path in paths.rglob('*'):
+        if path.is_file():
+            files.append(File(path))
+    return files
+
 def main():
 	print('hello world!')
+	files = get_manu_files()
+	print(f'files names to convert: {f.name for f in files}')
 
 main()
